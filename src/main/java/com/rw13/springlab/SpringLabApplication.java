6307b2592e8f95112e67449d5fe0901d3b4b600b
RazorWire13 <baneblade13@gmail.com>
2018-12-11 11:26:53 -0800

    lab features complete with tests

--- src/main/java/com/rw13/springlab/SpringLabApplication.java
+++ src/main/java/com/rw13/springlab/SpringLabApplication.java
@@ -8,5 +8,7 @@ public class SpringLabApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLabApplication.class, args);
+		System.out.println("http://localhost:8080/hello");
+		System.out.println("http://localhost:8080/capitalize/hello?name=heyo%20what%20is%20up%20party%20people");
 	}
 }
